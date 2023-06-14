14ea5fb7cc31d7747a64d6277c27ab8518e8bbf6
nninnnin <nninnnin7@gmail.com>
2023-06-14 23:51:52 +0900

    Add settings for deploying contract to public network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { config } = require("dotenv");
+config();
+
+const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
